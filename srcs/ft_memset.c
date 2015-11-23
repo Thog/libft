91fa26f1e6d6b337bcc02ac5189e063be1c9ddc0
Thomas GUILLEMARD <tguillem@e3r4p11.42.fr>
2015-11-23 17:23:48 +0100

    Use NULL instead of 0 to return a correct pointer

--- srcs/ft_memset.c
+++ srcs/ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: tguillem <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/23 12:45:01 by tguillem          #+#    #+#             */
-/*   Updated: 2015/11/23 13:12:08 by tguillem         ###   ########.fr       */
+/*   Updated: 2015/11/23 17:14:37 by tguillem         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	*ft_memset(void *b, int c, size_t len)
 
 	str = (unsigned char *)b;
 	if (str == NULL)
-		return (0);
+		return (NULL);
 	while (len--)
 		*str++ = c;
 	return (b);
