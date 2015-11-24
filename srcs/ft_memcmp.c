0a9f12e4c4034d2ed80e55468e9935c5f83cfec0
Thomas GUILLEMARD <tguillem@student.42.fr>
2015-11-24 10:10:35 +0100

    Add ft_memcmp and fix norme in ft_memccpy

--- srcs/ft_memcmp.c
+++ srcs/ft_memcmp.c
@@ -1,33 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memccpy.c                                       :+:      :+:    :+:   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tguillem <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2015/11/24 08:53:24 by tguillem          #+#    #+#             */
-/*   Updated: 2015/11/24 10:10:09 by tguillem         ###   ########.fr       */
+/*   Created: 2015/11/24 09:30:03 by tguillem          #+#    #+#             */
+/*   Updated: 2015/11/24 10:09:46 by tguillem         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memccpy(void *s1, const void *s2, int c, size_t n)
+int	memcmp(const void *s1, const void *s2, size_t n)
 {
-	size_t			i;
-	unsigned char	*result;
-	unsigned char	*source;
-	unsigned char	limit;
+	unsigned char	*str1;
+	unsigned char	*str2;
 
-	result = (unsigned char*)s1;
-	source = (unsigned char*)s2;
-	limit = (unsigned char)c;
-	i = 0;
-	while (i < n)
+	str1 = (unsigned char *)s1;
+	str2 = (unsigned char *)s2;
+	while (n--)
 	{
-		if ((*result++ = *source) == limit)
-			return (result);
-		i++;
+		if (*str1 != *str2)
+			return (str1 - str2);
+		str1++;
+		str2++;
 	}
-	return (NULL);
+	return (0);
 }
