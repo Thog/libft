d3a3720ca8272548c1867625f38f8858ad11bee6
Thomas GUILLEMARD <tguillem@student.42.fr>
2015-11-27 10:39:01 +0100

    Fix possible case of segfault

--- srcs/ft_lstmap.c
+++ srcs/ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: tguillem <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/26 12:03:38 by tguillem          #+#    #+#             */
-/*   Updated: 2015/11/27 10:32:01 by tguillem         ###   ########.fr       */
+/*   Updated: 2015/11/27 10:38:39 by tguillem         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,18 +20,18 @@ t_list		*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
 	t_list	*prev;
 
 	result = f(lst);
-	result = ft_lstnew(result->content, result->content_size);
 	if (result == NULL)
 		return (NULL);
+	result = ft_lstnew(result->content, result->content_size);
 	result->next = NULL;
 	prev = result;
 	cursor = lst->next;
 	while (cursor != NULL)
 	{
 		tmp = f(cursor);
-		tmp = ft_lstnew(tmp->content, tmp->content_size);
 		if (tmp == NULL)
 			break ;
+		tmp = ft_lstnew(tmp->content, tmp->content_size);
 		prev->next = tmp;
 		prev = tmp;
 		cursor = cursor->next;
