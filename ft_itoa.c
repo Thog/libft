c9b623763390825386f7b35deee327722bbde0f4
Thomas GUILLEMARD <tguillem@student.42.fr>
2015-12-07 15:46:39 +0100

    Rewrite itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,58 +6,45 @@
 /*   By: tguillem <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/23 12:44:50 by tguillem          #+#    #+#             */
-/*   Updated: 2015/11/30 08:32:30 by tguillem         ###   ########.fr       */
+/*   Updated: 2015/12/07 12:53:52 by tguillem         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static void		set(int *a, int *b, int new_a, int new_b)
+static int		countdigit(int n)
 {
-	*a = new_a;
-	*b = new_b;
-}
+	int		result;
 
-static void		ft_setintchar(char **a, int *b, char *new_a, int new_b)
-{
-	*a = new_a;
-	*b = new_b;
-}
-
-static void		ft_init(int *n, int *sign, int *exp, int *size)
-{
-	set(exp, size, 1, 0);
-	*sign = *n < 0 ? 1 : 0;
-	*n *= *sign ? -1 : 1;
-	while (*n >= *exp && *n < MAX)
-		set(exp, size, *exp * 10, *size + 1);
-	*exp = *n ? *exp / 10 : *exp;
-	*size += (*sign || !*n) ? 1 : 0;
+	result = 0;
+	while (n)
+	{
+		result++;
+		n /= 10;
+	}
+	return (result);
 }
 
 char			*ft_itoa(int n)
 {
-	int		exp;
-	int		size;
-	int		sign;
-	int		i;
 	char	*result;
+	int		sign;
+	int		digit;
 
-	ft_init(&n, &sign, &exp, &size);
-	if (!(result = (n == MIN ? ft_strdup("-2147483648") :
-					ft_strnew(size)))
-			|| (result && n == MIN)
-			|| !(result = n == MAX ? ft_strdup("2147483647") : result)
-			|| (result && n == MAX))
-		return (result);
-	result[size] = '\0';
-	if (sign)
-		*result++ = '-';
-	while (exp >= 1)
+	if (!n)
+		return (ft_strdup("0"));
+	sign = n < 0 ? -1 : 1;
+	digit = countdigit(n) + (sign == -1);
+	result = ft_strnew(digit);
+	if (!result)
+		return (NULL);
+	result += digit;
+	while (n)
 	{
-		i = (n / exp);
-		set((int *)result, &n, i + '0', n - i * exp);
-		ft_setintchar(&result, &exp, result + 1, exp / 10);
+		*--result = '0' + ((n % 10) * sign);
+		n /= 10;
 	}
-	return (result - size);
+	if (sign == -1)
+		*--result = '-';
+	return (result);
 }
