2ae779bed5556c6640a698de91fd03d557e46b01
Thomas GUILLEMARD <tguillem@student.42.fr>
2015-12-09 11:21:12 +0100

    Use malloc to avoid useless bzero

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: tguillem <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/24 08:51:47 by tguillem          #+#    #+#             */
-/*   Updated: 2015/12/09 10:26:03 by tguillem         ###   ########.fr       */
+/*   Updated: 2015/12/09 10:58:04 by tguillem         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 
 	if (dst == src || !len)
 		return (dst);
-	buffer = (unsigned char *)ft_memalloc(len);
+	buffer = (unsigned char *)malloc(len);
 	ft_memcpy(buffer, src, len);
 	ft_memcpy(dst, buffer, len);
 	free(buffer);
