ddab931c3411f0e6390500e4307c6d5ea25cfc18
Thomas GUILLEMARD <tguillem@student.42.fr>
2015-12-09 10:35:26 +0100

    Use malloc in ft_memmove to move buffer >= 128Mb

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: tguillem <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/24 08:51:47 by tguillem          #+#    #+#             */
-/*   Updated: 2015/11/24 09:26:14 by tguillem         ###   ########.fr       */
+/*   Updated: 2015/12/09 10:26:03 by tguillem         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,13 @@
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	unsigned char buffer[len];
+	unsigned char *buffer;
 
+	if (dst == src || !len)
+		return (dst);
+	buffer = (unsigned char *)ft_memalloc(len);
 	ft_memcpy(buffer, src, len);
 	ft_memcpy(dst, buffer, len);
+	free(buffer);
 	return (dst);
 }
