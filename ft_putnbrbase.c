6400f19d09a43fbf91a7dff6dec415dcc2228047
Thomas GUILLEMARD <tguillem@student.42.fr>
2016-01-26 15:24:49 +0100

    Fix Norme

--- ft_putnbrbase.c
+++ ft_putnbrbase.c
@@ -1,4 +1,15 @@
-#include <unistd.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbrbase.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tguillem <tguillem@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/01/26 15:23:36 by tguillem          #+#    #+#             */
+/*   Updated: 2016/01/26 15:23:44 by tguillem         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 static void	ft_putnbrbase_inner(uintmax_t nbr, char *base, size_t baselen)
