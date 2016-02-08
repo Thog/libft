196d8c03d0d94996bb3e12b69709689264938705
Thog <thog92@hotmail.fr>
2016-02-08 10:25:26 +0100

    Use write directly in ft_putstr

--- ft_putnbrbase.c
+++ ft_putnbrbase.c
@@ -5,11 +5,12 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: tguillem <tguillem@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/01/26 15:23:36 by tguillem          #+#    #+#             */
-/*   Updated: 2016/01/26 15:23:44 by tguillem         ###   ########.fr       */
+/*   Created: 2016/02/04 13:02:08 by tguillem          #+#    #+#             */
+/*   Updated: 2016/02/04 13:02:10 by tguillem         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
 #include "libft.h"
 
 static void	ft_putnbrbase_inner(uintmax_t nbr, char *base, size_t baselen)
