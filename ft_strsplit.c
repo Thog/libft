7c585b1caa5f3ef38b44f1f5b113e473a2a8c8b9
Thomas GUILLEMARD <tguillem@student.42.fr>
2016-03-14 13:21:07 +0100

    Fix Norme

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,17 +6,17 @@
 /*   By: tguillem <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/25 09:59:45 by tguillem          #+#    #+#             */
-/*   Updated: 2015/12/04 10:58:58 by tguillem         ###   ########.fr       */
+/*   Updated: 2016/03/14 13:18:35 by tguillem         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdlib.h>
 
-static size_t	count_words(const char *str, char separator)
+static size_t		count_words(const char *str, char separator)
 {
 	size_t		result;
-	int		is_word;
+	int			is_word;
 
 	result = 0;
 	is_word = 0;
@@ -34,7 +34,7 @@ static size_t	count_words(const char *str, char separator)
 	return (result);
 }
 
-static size_t word_length(const char *str, char separator)
+static size_t		word_length(const char *str, char separator)
 {
 	int		length;
 
@@ -47,16 +47,14 @@ static size_t word_length(const char *str, char separator)
 	return (length);
 }
 
-char			**ft_strsplit(char const *s, char c)
+char				**ft_strsplit(char const *s, char c)
 {
 	char	**result;
 	size_t	word_count;
-	size_t	i;
 	size_t	length;
 
 	if (s)
 	{
-		i = 0;
 		length = 0;
 		word_count = count_words(s, c);
 		if ((result = (char **)ft_memalloc(sizeof(char*) * (word_count + 1))))
@@ -66,13 +64,11 @@ char			**ft_strsplit(char const *s, char c)
 				while (*s == c && *s)
 					s++;
 				length = word_length(s, c);
-				result[i] = ft_strsub(s, 0, length);
-				if (!result[i])
+				if (!(*result++ = ft_strsub(s, 0, length)))
 					return (NULL);
 				s += length;
-				i++;
 			}
-			result[i] = NULL;
+			*result = NULL;
 			return (result);
 		}
 	}
